f6ecce0c4f914f73ec2307dfe5fd28004e2519f1
Zach Weinberg <weinberz@gmail.com>
2016-01-29 17:47:33 -0500

    Fixing rise and fall fit.

--- fallFit.m
+++ fallFit.m
@@ -1,18 +1,13 @@
 function [fitted_fall fgof] = fallFit(track) %(TP) n should be track number in ProcessedTracks
   iv = track.A;
-  x1 = [iv((find(iv==max(iv))):end)];
-  x1 = find(~isnan(x1));
-  y1 = [1:numel(x1)]*0.1;
-  y1 = y1(find(~isnan(x1)));
-
-  ymax = mean(y1(numel(x1):end));
-  ybase = min(y1(numel(x1):end));
-  
+  y1 = [iv((find(iv==max(iv))):end)];
+  x1 = [1:numel(y1)]*0.1;
+  y1 = y1(find(~isnan(y1)));
+  x1 = x1(find(~isnan(y1)));
 
   ft = fittype( 'power1' );
   opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
   opts.Display = 'Off';
-  opts.StartPoint = [ybase, ymax];
 
   if numel(x1)<2
       fitted_fall = [];
--- riseFit.m
+++ riseFit.m
@@ -1,24 +1,17 @@
 function [fitted_rise rgof] = riseFit(track) %(TP) n should be track number in ProcessedTracks
   iv = track.A;
-  x1 = [iv(1:(find(iv==max(iv))))];
-  x1 = find(~isnan(x1));
-  y1 = [1:numel(x1)]*0.1;
-  y1 = y1(find(~isnan(x1)));
-
-  ymax = mean(y1(1:numel(x1)));
-  ybase = min(y1(1:numel(x1)));
-  
-  ymax = find(~isnan(ymax));
-  ybase = ybase(find(~isnan(ymax)));
+  y1 = [iv(1:(find(iv==max(iv))))];
+  x1 = [1:numel(y1)]*0.1;
+  y1 = y1(find(~isnan(y1)));
+  x1 = x1(find(~isnan(y1)));
 
   ft = fittype( 'exp1' );
   opts = fitoptions( 'Method', 'NonlinearLeastSquares' );
-  opts.Display = 'Off';
-  opts.StartPoint = [ybase, ymax]
-  
+  opts.Display = 'final';
+
   if numel(x1)<2
       fitted_rise = [];
       rgof = struct('rsquare', NaN);
   else
-      [fitted_rise rgof] = fit( x1', y1', ft, opts );
+      [fitted_rise rgof] = fit(x1', y1', ft, opts );
   end
