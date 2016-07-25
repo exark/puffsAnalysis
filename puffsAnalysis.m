22fbd8c20599fab50bd769f90040607d15c06e78
Tiffany <tiffanyphan1221@gmail.com>
2016-07-25 13:57:59 -0400

    track processing with ampltitude

--- puffsAnalysis.m
+++ puffsAnalysis.m
@@ -58,7 +58,7 @@
 
 % Classification: 
 % ----------------------------
-% 1) Through much experimentation, we found that classification with parameters pallAdiff, pfallR2 and pvp yielded 
+% 1) Through much experimentation, we found that classification with parameters pallAdiff, pfallR2, pvp, tnpeaks and npeaks yielded 
 % best results and strongly recommend staying with these to build the classifier. 
 % 2) The classification code was written using Python 3.5 and many of Python's additional libraries. 
 %    Download the Anaconda distribution of Python to ensure that that your machine has all the necessary packages. 
@@ -114,13 +114,14 @@ runTracking(data, settings, opts{:});
 %-------------------------------------------------------------------------------
 % 3) Track processing
 %-------------------------------------------------------------------------------
-runPuffTrackProcessing(data, 'Overwrite', false);
+runPuffTrackProcessing(data, 'Overwrite', true);
 
 %-------------------------------------------------------------------------------
 % 4) Random Forest Classification using Python
 %-------------------------------------------------------------------------------
 runPuffClassification(data, 'RF classifier', 'IsTraining', false,...
-                      'SecondFile', 'C:\Users\tiffany\Downloads\[RunX]SpH MOR example puff movie\Cell1_0.1s\Ch1\Classification\processedTracks.npy');
+    'SecondFile', 'C:\Users\tiffany\Downloads\[RunX]SpH MOR example puff movie\Cell1_0.1s\Ch1\Classification\ProcessedTracks.npy');
+                     
                      
 
 res = []; % (ZW) This is a place holder for the return value until we have a runPuffsAnalysis or similar
